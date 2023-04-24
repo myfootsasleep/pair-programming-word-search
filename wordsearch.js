a342977e0c849dc737edacdd6a1ebb3e332ca7a2
myfootsasleep <richardoda@gmail.com>
2023-04-23 20:06:44 -0700

    adding wordsearch.js pair programming with @Su0112

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let row = [];
+    for (let x = 0; x < letters.length; x++) {
+      row.push(letters[x][i]);
     }
-}
+    verticalJoin.push(row);
+  }
+  //join the verticle
+  const newVertJoin = verticalJoin.map(ls => ls.join(''));
+  
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (l of newVertJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
+
+//Pair Programming with @Su0112(Shruthi Neeraj) and @myfootasleep(Richard)
